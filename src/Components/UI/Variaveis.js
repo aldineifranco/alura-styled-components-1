d82b9cd2e44a348751dec887d8425d6ffc23aac7
Aldinei <aldinei.franco@unimedprudente.com.br>
2022-10-25 16:31:02 -0300

    add projeto

---
+++ src/Components/UI/Variaveis.js
@@ -0,0 +1,9 @@
+export const corPrimaria = "#41d3be";
+
+export const fundoClaro = "#f1f1f1";
+export const conteudoClaro = "white";
+export const textoFundoClaro = "#3e3e3e";
+
+export const fundoEscuro = "#2e2e2e";
+export const conteudoEscuro = "#3e3e3e";
+export const textoFundoEscuro = "white";
